224190e9ed1bde5f201165982157592ed04f78cb
Slumtoto <martinslumtotos@gmail.com>
2025-04-02 12:05:28 +0300

    yes

--- app.py
+++ app.py
@@ -1,72 +1,4 @@
-@app.route('/')
-def home():
-    return render_template('yy.html')  # Connects to HTML file
-@app.route('/')
-def home():
-    return render_template('index.html')  # Connects to HTML file
-
-    
-    return render_template('shop.html', products=products)
-from flask import Flask, render_template, redirect, url_for, request, flash
-from models import db, User, Product, bcrypt
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
-app.config['SECRET_KEY'] = 'your_secret_key'
-
-db.init_app(app)
-bcrypt.init_app(app)
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.route('/')
-def home():
-    products = Product.query.all()
-    return render_template('index.html', products=products)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User(username=username)
-        user.set_password(password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registration successful. Please log in.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.check_password(password):
-            login_user(user)
-            return redirect(url_for('home'))
-        else:
-            flash('Login unsuccessful. Please check your username and password.', 'danger')
-    return render_template('login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
@@ -79,17 +11,25 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+# User Model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+# Product Model
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     image = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=True)
+
+# Cart Model
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -108,6 +48,7 @@ def register():
         new_user = User(username=username, password=password)
         db.session.add(new_user)
         db.session.commit()
+        flash('Account created successfully! Please log in.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html')
 
@@ -120,6 +61,8 @@ def login():
         if user and bcrypt.check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check your username and password.', 'danger')
     return render_template('login.html')
 
 @app.route('/logout')
@@ -132,6 +75,39 @@ def logout():
 def product_detail(product_id):
     product = Product.query.get_or_404(product_id)
     return render_template('product.html', product=product)
+
+@app.route('/cart')
+@login_required
+def cart():
+    cart_items = db.session.query(Cart, Product).join(Product).filter(Cart.user_id == current_user.id).all()
+    total = sum(item.Product.price * item.Cart.quantity for item in cart_items)
+    return render_template('cart.html', cart_items=cart_items, total=total)
+
+@app.route('/add_to_cart/<int:product_id>', methods=['POST'])
+@login_required
+def add_to_cart(product_id):
+    product = Product.query.get_or_404(product_id)
+    existing_cart_item = Cart.query.filter_by(user_id=current_user.id, product_id=product.id).first()
+    if existing_cart_item:
+        existing_cart_item.quantity += 1
+    else:
+        new_cart_item = Cart(user_id=current_user.id, product_id=product.id)
+        db.session.add(new_cart_item)
+    db.session.commit()
+    flash(f'Added {product.name} to your cart.', 'success')
+    return redirect(url_for('cart'))
+
+@app.route('/remove_from_cart/<int:cart_id>', methods=['POST'])
+@login_required
+def remove_from_cart(cart_id):
+    cart_item = Cart.query.get_or_404(cart_id)
+    if cart_item.user_id == current_user.id:
+        db.session.delete(cart_item)
+        db.session.commit()
+        flash('Item removed from your cart.', 'success')
+    else:
+        flash('Unauthorized action.', 'danger')
+    return redirect(url_for('cart'))
 
 if __name__ == '__main__':
     with app.app_context():

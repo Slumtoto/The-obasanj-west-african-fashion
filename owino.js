316e6f2ea353b0aee25a079d9f7f2d963e74828b
Slumtoto <martinslumtotos@gmail.com>
2025-04-11 15:42:21 +0300

    fff

--- owino.js
+++ owino.js
@@ -4,7 +4,7 @@ let container = document.querySelector('.container');
 let close = document.querySelector('.close');
 
 iconcart.addEventListener('click', () => {
-    if (cart.style.right === '-100%') {
+    if (cart.style.right === '-100%' || cart.style.right === '') {
         cart.style.right = '0';
         container.style.transform = 'translateX(-400px)';
     } else {
@@ -21,10 +21,11 @@ close.addEventListener('click', () => {
 let products = null;
 
 fetch('product.json')
-    .then(response => response.json()) // fixed .jason()
+    .then(response => response.json())
     .then(data => {
         products = data;
         addDataToHTML();
+        addExtraProducts();
     });
 
 function addDataToHTML() {
@@ -36,10 +37,26 @@ function addDataToHTML() {
         item.classList.add('item');
         item.innerHTML = `
             <img src="${product.image}" alt="">
-            <h2>${product.name}</h2>
+            <h2>${product.Name}</h2>
             <div class="price">$${product.price}</div>
             <button>add to cart</button>
         `;
         listproductHTML.appendChild(item);
     });
 }
+
+function addExtraProducts() {
+    let listproductHTML = document.querySelector('.listproduct');
+
+    const newProduct = document.createElement('div');
+    newProduct.classList.add('item');
+    newProduct.innerHTML = `
+        <img src="the obasanjo west african fashion img6.jpg" alt="">
+        <h2>Product Name</h2>
+        <div class="price">$550</div>
+        <button>add to cart</button>
+    `;
+    listproductHTML.appendChild(newProduct);
+}
+
+
